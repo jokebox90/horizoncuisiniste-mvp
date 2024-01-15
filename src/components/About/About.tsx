f52910129e3d7286289f9dc736cc8a5826f65b98
Jokebox90 <dev.jvagnier@gmail.com>
2024-01-15 21:17:39 +0100

    Typo

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -23,18 +23,18 @@ export default function About() {
           <h2 className={styles.title}>Qui sommes-nous ?</h2>
           <p className={styles.text}>
             Situés au cœur de la dynamique ville, nous sommes fiers de porter le
-            nom de 'BONDY' comme une marque. Bondy est une collectivité engagée
+            nom de &apos;BONDY&apos; comme une marque. Bondy est une collectivité engagée
             avec vitalité et énergie dans un mode de vie basé sur le bien-être
-            et le progrès. Notre philosophie s'inspire de sa solidité, de sa
+            et le progrès. Notre philosophie s&apos;inspire de sa solidité, de sa
             persévérance et de son dévouement, Nous portons toutes ces valeurs
             dans chacun des projets pour créer des espaces equilibrés entre
             élégance et prospérité.
           </p>
           <p className={styles.text}>
-            Le terme 'BUILDER', associé à notre nom, représente notre conscience
+            Le terme &apos;BUILDER&apos;, associé à notre nom, représente notre conscience
             détails et notre passion pour des constructions raffinées. Chaque
-            espace que nous concevons est le fruit d'une quête harmonieuse
-            d'excellence. Ils reflétant notre promesse de créer des intérieurs
+            espace que nous concevons est le fruit d&apos;une quête harmonieuse
+            d&apos;excellence. Ils reflétant notre promesse de créer des intérieurs
             où chaque élément est en parfaite symbiose avec le confort qui fait
             votre fierté.
           </p>
