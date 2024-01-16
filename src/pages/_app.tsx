5de2237299defae88e7cea0e5d16d7cf27ffc48a
Jokebox90 <dev.jvagnier@gmail.com>
2024-01-16 04:13:02 +0100

    Ajoute les en-tête SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter, Josefin_Sans, Josefin_Slab, Playball } from "next/font/google";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 const fontJosefinSans = Josefin_Sans({
@@ -28,8 +30,21 @@ const mainClasses = [
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main id="main" className={mainClasses}>
-      <Component {...pageProps} />
-    </main>
+    <Fragment>
+      <Head>
+        <title>
+          BONDY BUILDER, sublimez votre intérieur avec une touche d'artisanat
+          unique
+        </title>
+        <meta
+          name="description"
+          content="Rencontrez l'équipe Bondy Builder, vos experts en rénovation intérieure à Bondy. Spécialisés dans la modernisation des installations électriques et de plomberie, nous transformons vos espaces de vie avec élégance et confort. Nos services incluent la peinture, la pose de revêtements muraux et de sols, la maçonnerie et plus encore. Faites confiance à notre équipe pour des solutions de décoration et de rénovation sur mesure, alliant qualité et attention aux détails. Contactez-nous pour redéfinir votre espace avec style et fonctionnalité"
+        />
+      </Head>
+
+      <main id="main" className={mainClasses}>
+        <Component {...pageProps} />
+      </main>
+    </Fragment>
   );
 }
