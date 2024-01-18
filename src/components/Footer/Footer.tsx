084438d81364dc8e54fc40a20c6339ff946df449
Jokebox90 <dev.jvagnier@gmail.com>
2024-01-18 05:40:03 +0100

    Configure les liens vers les réseaux sociaux

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,14 @@
 // src/components/Footer/Footer.tsx
 
 import styles from "@/components/Footer/Footer.module.css";
-import { faFacebook, faInstagram, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -10,7 +16,7 @@ export default function Footer() {
       <div className={styles.box}>
         <div className={styles.body}>
           <div className={styles.item}>
-            <p>&copy; Auteur</p>
+            <p>&copy; Copyright</p>
             <div className="w-16 h-1 mt-3 mb-4 mx-auto bg-amber-500 rounded-full"></div>
             <p>
               <span className="font-semibold">BONDY BUILDER</span>
@@ -20,21 +26,27 @@ export default function Footer() {
             <p>Suivez-nous sur les réseaux</p>
             <div className="w-16 h-1 mt-3 mb-4 bg-amber-500 rounded-full"></div>
             <div className="inline-flex gap-2">
-              <FontAwesomeIcon
-                icon={faFacebook}
-                className="w-8 h-8 text-blue-600"
-              />
+              <Link
+                href={`https://www.facebook.com/profile.php?id=61555889212804`}
+                target="_blank"
+                className="w-8 h-8 text-blue-600 hover:scale-105 active:scale-95"
+              >
+                <FontAwesomeIcon icon={faFacebook} />
+              </Link>
               <FontAwesomeIcon
                 icon={faInstagram}
-                className="w-8 h-8 text-pink-600"
+                // className="w-8 h-8 text-pink-600"
+                className="w-8 h-8 text-zinc-500"
               />
               <FontAwesomeIcon
                 icon={faLinkedin}
-                className="w-8 h-8 text-cyan-600"
+                // className="w-8 h-8 text-cyan-600"
+                className="w-8 h-8 text-zinc-500"
               />
               <FontAwesomeIcon
                 icon={faXTwitter}
-                className="w-8 h-8 text-zinc-50"
+                // className="w-8 h-8 text-zinc-50"
+                className="w-8 h-8 text-zinc-500"
               />
             </div>
           </div>
